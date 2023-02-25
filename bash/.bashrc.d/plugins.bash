52beefe117c3b36f3bb8c96cb7fa8dd13e2b117d
Cameron Balahan <thecameron@gmail.com>
2023-02-25 17:13:36 -0500

    Add plugin for bash_completion

---
+++ bash/.bashrc.d/plugins.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# homebrew
+hash brew >/dev/null 2>&1 && src_files "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+
+# unclutter your .profile: load/unload env depending on the current directory
+# https://direnv.net/
+#hash direnv >/dev/null 2>&1 && eval "$(direnv hook bash)"
+
+# github's official command line tool
+#hash gh >/dev/null 2>&1 && eval "$(gh completion -s bash)"
+
+# a smarter cd command
+# https://github.com/ajeetdsouza/zoxide
+#hash zoxide >/dev/null 2>&1 && eval "$(zoxide init bash)"
